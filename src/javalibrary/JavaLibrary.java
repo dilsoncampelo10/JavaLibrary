2c8bae9a0f1ac0786de1bc8af65c96ce224672ff
dilsoncampelo10 <dilsono316@gmail.com>
2023-08-04 22:51:17 -0300

    initialr with first models

---
+++ src/javalibrary/JavaLibrary.java
@@ -0,0 +1,17 @@
+
+package javalibrary;
+
+/**
+ *
+ * @author dilson
+ */
+public class JavaLibrary {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo");
+    }
+    
+}
---
+++ src/javalibrary/models/User.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javalibrary.models;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author dilson
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private LocalDateTime birthdate;
+    private String username;
+    private String password;
+}
